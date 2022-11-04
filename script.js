c7bec57b82901d34ca70d12fd66fa8740ce4deb5
Olulanke-Mainasara <95680556+Olulanke-Mainasara@users.noreply.github.com>
2022-11-04 11:54:52 +0100

    Added four new calculators

--- script.js
+++ script.js
@@ -20,4 +20,12 @@ function updateRate() {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval + "%";
 }
+
+new Glide(".glide", {
+    startAt: 2,
+    perView: 2,
+    gap: 300,
+    animationDuration: 1000,
+    focusAt: 'center',
+}).mount();
         
